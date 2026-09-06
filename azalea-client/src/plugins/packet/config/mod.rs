9e709b5d4eb6ffb381d4cd6155967ed34acce79f
Stepan Klymonchuk <stepanstepan0000@gmail.com>
2026-09-06 18:50:38 +0200

    fix: echo back minecraft:core known pack in select_known_packs handler

--- azalea-client/src/plugins/packet/config/mod.rs
+++ azalea-client/src/plugins/packet/config/mod.rs
@@ -209,13 +209,17 @@ impl ConfigPacketHandler<'_> {
     pub fn select_known_packs(&mut self, p: &ClientboundSelectKnownPacks) {
         debug!("Got select known packs packet {p:?}");
 
+        // Echo back the minecraft:core known pack so the server knows we have
+        // vanilla registry data and skips sending full NBT payloads.
+        let known_packs: Vec<KnownPack> = p.known_packs.iter()
+            .filter(|kp| kp.namespace == "minecraft" && kp.id == "core")
+            .cloned()
+            .collect();
+
         as_system::<Commands>(self.ecs, |mut commands| {
-            // resource pack management isn't implemented
             commands.trigger(SendConfigPacketEvent::new(
                 self.player,
-                ServerboundSelectKnownPacks {
-                    known_packs: vec![],
-                },
+                ServerboundSelectKnownPacks { known_packs },
             ));
         });
     }
